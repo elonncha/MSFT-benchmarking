8867fd3ec46f1a56fd0ae5ad2f359dee964ad42e
Elon Cha <54544200+elonncha@users.noreply.github.com>
2022-07-18 01:40:00 -0400

    Update download.py

--- python/download.py
+++ python/download.py
@@ -4,7 +4,7 @@ download_CDCPlaces([2018,2019,2020,2021])
 
 download_ACS(year = 2019, api_key = 'd54b04fce5ead0b754d8951da1ced097f3d050e1',
                  table = 'detailed',
-                 state_id = '13', county_id = ['135'],
+                 state_id = '13', county_id = ['121', '089'],
                  acs5_variable_list = ("B01003_001E", "B19013_001E"), acs_name_list = ('pop_total', 'medhhinc_all')
                  )
 
